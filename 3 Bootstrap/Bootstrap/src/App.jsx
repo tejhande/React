a12fee69795c47d70e2d712f0eada2abcf58e75d
tejhande <59686002+tejhande@users.noreply.github.com>
2024-06-02 00:59:55 +0530

    3

---
+++ 3 Bootstrap/Bootstrap/src/App.jsx
@@ -0,0 +1,22 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+      <div>
+        <ul class="list-group">
+  <li class="list-group-item active" aria-current="true">An active item</li>
+  <li class="list-group-item">A second item</li>
+  <li class="list-group-item">A third item</li>
+  <li class="list-group-item">A fourth item</li>
+  <li class="list-group-item">And a fifth one</li>
+</ul>
+      </div>
+  )
+}
+
+export default App
